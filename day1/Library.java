e560cfc597e09e28f53e264ab1ac7c5ad2940e66
Jobita0106 <132143707+Jobita0106@users.noreply.github.com>
2025-11-21 11:16:34 +0530

    Add files via upload
    week2.day1 Assignment

---
+++ day1/Library.java
@@ -0,0 +1,28 @@
+package week2.day1;
+
+public class Library {
+	
+	public String addBook(String bookTitle) {
+		
+	        System.out.println("Book added successfully");
+	        
+	        return bookTitle;   
+	    }
+ 
+	    public void issueBook() {
+			    	
+	        System.out.println("Book issued successfully");
+	    }
+ 
+	    public static void main(String[] args) {
+
+	        Library mylibrary = new Library();
+
+	        mylibrary.addBook("Bridgerton");
+	       
+	        mylibrary.issueBook();
+	        
+	    }
+	    
+	}
+
